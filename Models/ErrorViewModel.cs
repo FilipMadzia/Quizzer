b76048fc39e7f3742b28b818079d57cf50ef3720
Filip Madzia <filipmadzia33@gmail.com>
2025-02-23 14:17:09 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quizzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
